9febab1ad3529d519f97d780b6a269b999339d57
John Knoll <jpknoll@ucdavis.edu>
2015-03-26 14:17:47 -0700

    add solution and proj

---
+++ TwitterDb/index.js
@@ -0,0 +1,24 @@
+﻿var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+});
+
+module.exports = {
+    create: function(obj) {
+        
+    },
+
+    getAll: function() {
+        
+    },
+
+    getById: function(id) {
+        
+    },
+
+
+};
